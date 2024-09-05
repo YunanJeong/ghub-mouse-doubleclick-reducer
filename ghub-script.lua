35b22bd11de3fce0179f9fb147348e5ba94191e6
YunanJeong <njhgle@gmail.com>
2024-09-06 02:25:45 +0900

    Resolve #1

--- ghub-script.lua
+++ ghub-script.lua
@@ -1,17 +1,22 @@
 -- 5: MOUSE FORWARD SIDE BUTTON
 -- 4: MOUSE GOBACK SIDE BUTTON
 FXXKED_BUTTON = 4
-gLastTime = "none"
-gCurTime = "none"
+IGNORE_INTERVAL_MS = 300
+
+gLastTime = 0
+gCurTime = 0
 gCount = 0
 
 function OnEvent(event, arg)
-  gCurTime = GetDate()
-  if event == "MOUSE_BUTTON_PRESSED" and arg == FXXKED_BUTTON then -- arg4번: 뒤로가기 물리버튼
-    if gLastTime == "-1" or (gCurTime ~= gLastTime) then
+  gCurTime = GetRunningTime()
+  local interval = gCurTime - gLastTime
+  if event == "MOUSE_BUTTON_PRESSED" and arg == FXXKED_BUTTON then
+    if gLastTime == 0 or (interval > IGNORE_INTERVAL_MS) then
       Log()
       GoBack() --TestPress()
       gLastTime = gCurTime
+    else
+      OutputLogMessage("\n> Ignore unintentional double-clicks that occured within %dms(<%dms)", interval, IGNORE_INTERVAL_MS)
     end
   end
 end
@@ -22,18 +27,18 @@ function Log()
     gCount = 1
   end
   OutputLogMessage("\n-----------------------------")
-  OutputLogMessage("\n> Last Click    : %s", gLastTime)
-  OutputLogMessage("\n> Current Click : %s", gCurTime)
-  OutputLogMessage("\n> goBack count  : %s", tostring(gCount))
+  OutputLogMessage("\n> Last Click    : %d", gLastTime)
+  OutputLogMessage("\n> Current Click : %d", gCurTime)
+  OutputLogMessage("\n> Count         : %d", gCount)
   OutputLogMessage("\n-----------------------------")
 end
 
 function GoBack()
-  PressKey("lalt") -- 뒤로가기 버튼 누르기
+  PressKey("lalt")
   Sleep(50)
   PressKey("left")
   Sleep(50)
-  ReleaseKey("lalt") -- 뒤로가기 버튼 떼기
+  ReleaseKey("lalt")
   Sleep(50)
   ReleaseKey("left")
   Sleep(50)
